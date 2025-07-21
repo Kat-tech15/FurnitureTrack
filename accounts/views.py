38e440b39ccfa05ea26366a388e83fabd1c4d02a
Kat-tech15 <kelvinkatwai@gmail.com>
2025-07-21 23:17:49 +0300

    added forms and templates

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render,redirect
 # Create your views here.
 
 def login_view(request):
-    return render(request, 'login.html')
+    return render(request, 'accounts/login.html')
 
 def register_view(request):
-    return render(request, 'reqister.html')
+    return render(request, 'accounts/reqister.html')
+
+def logout_view(request):
+    return render(request, 'accounts/logout.html')
