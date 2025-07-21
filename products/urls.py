9c38a83eb0a499eb6db96e527d5910dc58d6025c
Kat-tech15 <kelvinkatwai@gmail.com>
2025-07-21 23:19:41 +0300

    added forms and templates to products app

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('')
+    path('create_product/', views.create_product, name='create_product'),
+    path( 'product/<int:product_id>/update', views.update_product, name='update_product'),
+    path('product<int:product_id>/remove', views.remove_product, name='remove_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render, redirect
 
 
 # Create your views here.
+def create_product(request):
+    return render(request, 'products/create_product.html')
+
+def update_product(request):
+    return render(request, 'products/update_product.html')
+
+def remove_product(request):
+    return render(request, 'products/remove_product.html')
